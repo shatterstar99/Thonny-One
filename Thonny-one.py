88fe1be7c41fb4655647f204fb5c549860cdaa6c
shatterstar99 <32302069+shatterstar99@users.noreply.github.com>
2023-10-27 12:23:15 +0200

    Add files via upload
    Basically added 2 new functions and an incomplete third

--- Thonny-one.py
+++ Thonny-one.py
@@ -1,16 +1,84 @@
 from random import randint
-Deck = {1:["One",(1,"Heart","One"),(1,"Spade","One"),(1,"Club","One"),(1,"Diamond","One")],2:["Two",(2,"Heart","Two"),(2,"Spade","Two"),(2,"Club","Two"),(2,"Diamond","Two")],3:["Three",(3,"Heart","Three"),(3,"Spade","Three"),(3,"Club","Three"),(3,"Diamond","Three")],4:["Four",(4,"Heart","Four"),(4,"Spade","Four"),(4,"Club","Four"),(4,"Diamond","Four")],5:["Five",(5,"Heart","Five"),(5,"Spade","Five"),(5,"Club","Five"),(5,"Diamond","Five")],6:["Six",(6,"Heart","Six"),(6,"Spade","Six"),(6,"Club","Six"),(6,"Diamond","Six")],7:["Seven",(7,"Heart","Seven"),(7,"Spade","Seven"),(7,"Club","Seven"),(7,"Diamond","Seven")],8:["Eight",(8,"Heart","Eight"),(8,"Spade","Eight"),(8,"Club","Eight"),(8,"Diamond","Eight")],9:["Nine",(9,"Heart","Nine"),(9,"Spade","Nine"),(9,"Club","Nine"),(9,"Diamond","Nine")],10:["Ten",(10,"Heart","Ten"),(10,"Spade","Ten"),(10,"Club","Ten"),(10,"Diamond","Ten")],11:["Jack",(10,"Heart","Jack"),(10,"Spade","Jack"),(10,"Club","Jack"),(10,"Diamond","Jack")],12:["Queen",(10,"Heart","Queen"),(10,"Spade","Queen"),(10,"Club","Queen"),(10,"Diamond","Queen")],13:["King",(10,"Heart","King"),(10,"Spade","King"),(10,"Club","King"),(10,"Diamond","King")]}
+Deck = {1:["Ace",(1,11,"Heart","Ace"),(1,11,"Spade","Ace"),(1,11,"Club","Ace"),(1,11,"Diamond","Ace")],2:["Two",(2,"Heart","Two"),(2,"Spade","Two"),(2,"Club","Two"),(2,"Diamond","Two")],3:["Three",(3,"Heart","Three"),(3,"Spade","Three"),(3,"Club","Three"),(3,"Diamond","Three")],4:["Four",(4,"Heart","Four"),(4,"Spade","Four"),(4,"Club","Four"),(4,"Diamond","Four")],5:["Five",(5,"Heart","Five"),(5,"Spade","Five"),(5,"Club","Five"),(5,"Diamond","Five")],6:["Six",(6,"Heart","Six"),(6,"Spade","Six"),(6,"Club","Six"),(6,"Diamond","Six")],7:["Seven",(7,"Heart","Seven"),(7,"Spade","Seven"),(7,"Club","Seven"),(7,"Diamond","Seven")],8:["Eight",(8,"Heart","Eight"),(8,"Spade","Eight"),(8,"Club","Eight"),(8,"Diamond","Eight")],9:["Nine",(9,"Heart","Nine"),(9,"Spade","Nine"),(9,"Club","Nine"),(9,"Diamond","Nine")],10:["Ten",(10,"Heart","Ten"),(10,"Spade","Ten"),(10,"Club","Ten"),(10,"Diamond","Ten")],11:["Jack",(10,"Heart","Jack"),(10,"Spade","Jack"),(10,"Club","Jack"),(10,"Diamond","Jack")],12:["Queen",(10,"Heart","Queen"),(10,"Spade","Queen"),(10,"Club","Queen"),(10,"Diamond","Queen")],13:["King",(10,"Heart","King"),(10,"Spade","King"),(10,"Club","King"),(10,"Diamond","King")]}
+#Deck contains all the possible cards.
+Gamblers = {"Player":{"Hand":[],"Value": 0},"Dealer":{"Hand":[],"Value": 0}}
 Player = []
 #This is the players hand, he starts with zero cards.
+Playervalue = 0
+#This is the starting value of the players hand.
+Playerstring = []
+#This is a list that stores the hand in string format.
 Dealer = []
 #This is the players hand, he starts with zero cards.
+Dealervalue = 0
+#This is the starting value of the dealers hand
+Dealerstring = []
+#This is a list that stores the hand in string format.
+
+#Future idea: possibly put those global variables in a dict.
 
 def random(limit):
     randomvalue = randint(1,limit)
     return randomvalue
 #Function gets an input(like the length of the list
 #and returns a random value between 1 and the input.
-def dealCards():
-    Player.append[Deck[random(len(Deck))][random(4)]]
-    Player.append[Deck[random(len(Deck))][random(4)]]
 
+def dealCards(gambler):
+    gambler.append([Deck[random(len(Deck))][random(4)]])
+    #Add a drew this card message
+    
+#Deals out random cards from the deck and stores
+#in specified list
+    
+def valueofCards(gambler):
+    global Playervalue
+    for tpl in gambler:
+        gambler+string.append(f"{tpl[-1]} of {tpl[-2]}")
+        #Supposed to store the strings of tuple in list
+        #to print it out later
+        if len(tpl[0]) > 3: #Only the Ace tuples are longer than 3
+            if Playervalue + tpl[0][1] > 21:
+                Playervalue += tpl[0][0]
+                #if player would become bust when 11 is added,adds the value 1 instead,
+            else:
+                Playervalue += tpl[0][1]
+                #Otherwise Ace adds 11 points.
+        else:
+            Playervalue += tpl[0][0]
+    
+def hitorStay(Player):
+    print("Do you want to hit or stay?")
+    answer = input("1: Hit 2: Stay ")
+    if answer == "1":
+        print(f"Player draws another card.")
+        dealCards(Player)
+    elif answer == "2":
+        print(f"Player stays at the current hand, which is {Playervalue}.")
+    else:
+        print("/n")
+        print("Incorrect input please pick 1 or 2.")
+    #Options for the player after first draw.
+    #Call bust function after dealt cards.
+
+def hitorStayNPC(NPC):
+    #Try to make it so NPC string combine with value
+    #So that we get tex: Dealervalue when we input Dealer
+    if Dealervalue < 15 and > 10:
+        Hitchance = (2)
+        #50% chance of drawing another card
+    elif Dealervalue > 15:
+        Hitchance = (4)
+        #25% chance of drawing another card
+    elif Dealervalue > 17 and < 20
+        Hitchance = (100)
+        #1% chance of drawing another card
+    
+        
+    
+    
+        
+    
+    
+dealCards(Player)
+valueofCards(Player)
+print(Playervalue)
